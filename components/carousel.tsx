e8d3e9b4edcddd0990fcd5d9057dab9f1f464af0
Freddy González <7684330+fredoist@users.noreply.github.com>
2024-03-16 22:52:08 -0600

    hotfix

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Carousel() {
   return (
     <div className="my-12 w-full inline-flex flex-nowrap overflow-hidden">
-      <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+      <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll [&_img]:w-auto">
         {Array.from({ length: 9 }).map((_, index) => (
           <li key={`item-${index}`}>
             <Image
@@ -16,7 +16,7 @@ export default function Carousel() {
         ))}
       </ul>
       <ul
-        className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+        className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll [&_img]:w-auto"
         aria-hidden="true"
       >
         {Array.from({ length: 9 }).map((_, index) => (
