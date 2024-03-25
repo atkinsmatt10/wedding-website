7e641cef58a9a5c552b29f7d221c2ac303ce4880
Freddy González <7684330+fredoist@users.noreply.github.com>
2024-03-25 11:05:20 -0600

    hotfix

--- components/hero.tsx
+++ components/hero.tsx
@@ -29,7 +29,7 @@ export default function Hero() {
 
   return (
     <header
-      className="relative w-full h-[80vh] xl:h-[120vh] overflow-hidden"
+      className="relative w-full h-[65vh] xl:h-[120vh] overflow-hidden"
       id="top"
     >
       <motion.img
